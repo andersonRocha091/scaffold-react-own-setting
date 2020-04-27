2c3daf345958785ade420151395a7b60b14b1263
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-26 21:55:42 -0300

    Adicionando um header simples, e incluido na aplicação

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import { Route } from "react-router-dom";
 
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
 
 const App = () => {
   return (
     <div className="container-fluid">
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
     </div>
