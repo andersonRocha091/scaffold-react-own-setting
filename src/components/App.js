fcdd6e2339ecd338761784f1ea560f4d161b481e
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-26 22:03:43 -0300

    Adicionando switch no roteamento para garantir apenas uma chamada de rota, e tratar corretamente pagina nao encontrada

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
 
 const App = () => {
   return (
     <div className="container-fluid">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 };
