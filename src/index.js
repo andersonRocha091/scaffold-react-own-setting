c7b593829c5c25f6a9499dc63c0552ae7e9560c3
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-27 15:39:55 -0300

    Importando a store par uso em toda aplicação

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
+import configureStore from "./redux/ConfigureStore";
 import App from "./components/App";
 
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
