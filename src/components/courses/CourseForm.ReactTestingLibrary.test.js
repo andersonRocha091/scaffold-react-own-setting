be33ef309ad7b5dbb8694aaaee06fd750e2b78f4
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-30 16:22:22 -0300

    Testando com react-testing-library

---
+++ src/components/courses/CourseForm.ReactTestingLibrary.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { cleanup, render } from "react-testing-library";
+import CourseForm from "./CourseForm";
+
+afterEach(cleanup);
+
+function renderCourseForm(args) {
+  const defaultProps = {
+    authors: [],
+    course: {},
+    saving: false,
+    errors: {},
+    onSave: () => {},
+    onChange: () => {},
+  };
+
+  const props = { ...defaultProps, ...args };
+  return render(<CourseForm {...props} />);
+}
+
+it("Should render Add Course Header", () => {
+  const { getByText } = renderCourseForm();
+  getByText("Add Course");
+});
+
+it('Should label save button as "save" when not saving', () => {
+  const { getByText } = renderCourseForm();
+  getByText("Add Course");
+});
+
+it('Should label save button as "Saving..." when is saving', () => {
+  const { getByText, debug } = renderCourseForm({ saving: true });
+  debug();
+  getByText("Saving...");
+});
